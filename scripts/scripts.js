c098aa06cd6a5aebb2d83fb8261736692637405a
Kuznetsova Maria <luchik.sakuras@yandex.ru>
2025-04-21 12:11:34 +0300

    Add sections pricing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
 import Header from './Header.js'
 import VideoPlayerCollection from './VideoPlayer.js'
+import TabsCollection from './Tabs.js'
 
 new Header()
 new VideoPlayerCollection()
+new TabsCollection()
